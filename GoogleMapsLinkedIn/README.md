f9869d818d27f44ddffc48d58349a3dfe21f41de
Erick Ruiz <erickruiz29@gmail.com>
2014-12-07 15:07:31 -0800

    login integration

--- GoogleMapsLinkedIn/README.md
+++ GoogleMapsLinkedIn/README.md
@@ -1,83 +1,32 @@
-# Blank Intel XDK and Apache Cordova Project
+Login View App
+===================
+This template can be used for creating an app that requires login and registration before the app can be started, it shows option for login/register and takes you to login form view and register form view, this template can be used for creating applications similar to Facebook* app or Twitter* app.
 
-See [LICENSE.md][] for license terms and conditions.
+This basic HTML5 mobile app template is created using [Intel App Framework](https://github.com/01org/appframework), which is a free and open source Javascript framework targeted at HTML5 Mobile browsers with a blazingly fast query selector library and Mobile UI framework for creating app with native like UI and performance.
 
-Use this project as a starting point for an Intel XDK or Apache Cordova
-hybrid mobile app. One key file (`init-dev.js`) contains the
-initialization code needed to handle XDK device ready, Cordova device
-ready or browser document ready init events in a way that allows you to
-run your app in any of these environments. This init code works:
+You can use Intel AppFramework to create __mobile web apps__ or create __hybrid apps__ using tools like [_Intel XDK_](http://xdk-software.intel.com/) or _Adobe Phonegap build_ to build native packaged apps.
 
--   with the the Intel XDK Emulate tab
+App Framework has UI themes which look and feel similar to _iPhone_, _iPad_, _Android_, _Windows Phone_, _Blackberry 10_ and _Tizen_, the UI theme will be automatically applied by default when opened on these devices. Notice the screenshots below, the app code is same but the look and feel changes to match the device's native style.
 
--   in the XDK App Preview application (Test tab)
+![LoginViewApp](https://raw.github.com/gomobile/template-login-view/master/screenshot.png)
 
--   in the App Preview Crosswalk container (Debug tab)
+Intel(R) XDK
+-------------------------------------------
+This template is part of the Intel(R) XDK. 
+Download the Intel XDK at http://software.intel.com/en-us/html5.
+To see the technical detail of the template, please visit the article page 
+at http://software.intel.com/en-us/html5/articles/templates-to-get-started-with-html5-mobile-app-development. 
 
--   with the weinre debug script (Test tab)
+Application Files
+-----------------
+* app.json
+* index.html
+* README.md
+* app_framework/
 
--   in an app built using the Intel XDK legacy container (aka AppMobi
-    container)
 
--   in an app built using the standard Apache Cordova container (aka
-    Cordova CLI)
+App Framework (formerly jQ.Mobi)
+-----------------------------------------------------------------------------
+* source:  https://github.com/01org/appframework
+* license: https://github.com/01org/appframework/blob/master/license.txt
 
-When `init-dev.js` completes execution it issues a custom "`app.Ready`"
-event. Use this event to start your application, rather than waiting on
-"device ready" or "document ready" or "window load" or similar events.
-You should not have to modify anything in `init-dev.js` to use this
-code. Also, `init-dev.js` has been written so that it is not dependent
-on any external libraries or specific webviews. It has been tested with
-the following webviews and browsers:
-
--   Android 2.3, 4.0-4.3 and 4.4
-
--   iOS 6 and 7
-
--   Windows 8 Phone
-
--   Windows 8
-
--   Crosswalk
-
--   Chrome Desktop Browser
-
--   Internet Explorer 10 and 11
-
-This blank project works well for converting an existing web app into a
-hybrid app. One of the biggest issues encountered when porting a web app
-to a hybrid app is resolving the init sequence of the web app with the
-init sequence required of a hybrid HTML5 app. This gets especially
-difficult when large third-party libraries are part of the app. Due to
-the additional burden of initializing the underlying native code layer,
-developers sometimes have trouble getting their code that runs in a
-desktop browser to initialize in an HTML5 hybrid webview. Frequently
-this is due to the significant difference in resources between the
-desktop browser and the mobile webview (e.g., less memory, lower
-performance and a reduced feature set).
-
-You can combine `init-app.js` and `app.js` into a single file (e.g.,
-just `app.js`) and things will work just fine, as long as you start
-things up using the custom "`app.Ready`" event described above. Also,
-there is nothing particularly important about the `app.css` file, it
-contains a few global CSS definitions that are commonly applied to older
-Android devices, but certainly is not the "end all" for configuring the
-CSS in your hybrid HTML5 webview application.
-
-There are many comments in the files in this project. Please read those
-comments for details and further documentation. In particular, see the
-comments in the `index.html` file for recommendations on how to load
-your third-party libraries relative to your application code and the
-special hybrid libraries (`intelxdk.js`, `cordova.js` and `xhr.js`).
-
-There are a large number of `console.log()` messages contained within
-`init-dev.js`. They can be used to debug initialization problems and
-understand how the file works. It is highly recommended that you leave
-those `console.log()` messages in your app, they will not unduly slow
-down or burden your application.
-
-BTW: the "`dev`” prefix refers to "device" in this context, not
-"develop," because it grew out of a desire to build a more reliable and
-flexible "device ready" detector.
-
-  [LICENSE.md]: LICENSE.md
--- GoogleMapsLinkedIn/www/js/linkedin_map.js
+++ GoogleMapsLinkedIn/www/js/linkedin_map.js
@@ -83,90 +83,103 @@ $(".buttonClick").click(function(){
 var i = 0;
 //Success callback
 var suc = function(p) {
-    console.log("geolocation success", 4);
-
-    user = new LinkedInUser("https://www.linkedin.com/in/zhengyusun", p);
-
-    //Draws the map initially
-    if (_map === null) {
-        /*
-        currentLatitude = p.coords.latitude;
-        currentLongitude = p.coords.longitude;
-        */
-
-        /* LA - WESTWOOD latlng
-        currentLatitude = 34.056;
-        currentLongitude = -118.44;
-        */
-
-        currentLatitude = 37.7458;
-        currentLongitude = -122.4416;
-        drawMap();
-    } else {
-        myLatLng = new google.maps.LatLng(p.coords.latitude, p.coords.longitude);
-    }
-
-    // Hard-coded user results from the search
-    users = [
-        new LinkedInUser1("https://www.linkedin.com/pub/andrew-kuang/57/241/a93", p),
-        new LinkedInUser1("https://www.linkedin.com/in/ryanwmchan", p),
-        new LinkedInUser1("https://www.linkedin.com/in/zacharyli323", p)
-    ];
-    //console.log(users);
-
-    //Creates a new google maps marker object for using with the pins
-    while (i < users.length) {
-        //Create a new map marker
-        userLatLng = new google.maps.LatLng(users[i].lat, users[i].long);
-        var Marker = new google.maps.Marker({
-            position: userLatLng,
-            map: _map,
-            icon: 'http://i.imgur.com/NyckpY1.png'
-        });
-
-        console.log("Getting Linkedin info for user " + i);
-        IN.API.Profile("url=" + users[i].linkedInUrl)
-            .fields(["firstName", "lastName", "headline", "location", "industry", "picture-url", "public-profile-url"])
-            .result(function (result) {
-                console.log("result is");
-                console.log(result);
-                // Summary box for each searched user
-                var linkedinUserDivString = result.values[0]["firstName"] + ' ' + result.values[0]["lastName"] + '<br />'
-                                          + result.values[0]["headline"] + '<br />'
-                                          + result.values[0]["location"]["name"] + '<br />';
-
-                //Doesn't work for now (doesn't go into the first if statement
-                if (result.values[0]["pictureUrl"] == "undefined "){
-                    var linkedinUserImgDivString = '<img src="http://24sessions.com/img/profile_empty.jpg" />';
-                }
-                else{
-                    var linkedinUserImgDivString = '<img src="' + result.values[0]["pictureUrl"] + '" />';
-                }
-
-                linkedInResultStrings.push(linkedinUserDivString);
-                linkedInResultImgStrings.push(linkedinUserImgDivString);
-                //console.log(linkedInResultStrings);
-                //console.log(linkedInResultImgStrings);
-            })
-            .error(function (error) {
-                console.log(error);
-            });
-
-        google.maps.event.addListener(Marker, 'click', markerClickCallback(i));
-
-        if (_llbounds === null) {
-            //Create the rectangle in geographical coordinates
-            _llbounds = new google.maps.LatLngBounds(new google.maps.LatLng(p.coords.latitude, p.coords.longitude)); //original
+        console.log("geolocation success", 4);
+    
+        user = new LinkedInUser("https://www.linkedin.com/in/zhengyusun", p);
+    
+        //Draws the map initially
+        if (_map === null) {
+            /*
+            currentLatitude = p.coords.latitude;
+            currentLongitude = p.coords.longitude;
+            */
+            
+            /* LA - WESTWOOD latlng
+            currentLatitude = 34.056;
+            currentLongitude = -118.44;
+            */
+            
+            currentLatitude = 37.7458;
+            currentLongitude = -122.4416;
+            drawMap();
         } else {
-            //Extends geographical coordinates rectangle to cover current position
-            //_llbounds.extend(userLatLng);
+            myLatLng = new google.maps.LatLng(p.coords.latitude, p.coords.longitude);
         }
-        //Sets the viewport to contain the given bounds & triggers the "zoom_changed" event
-        //_map.fitBounds(_llbounds);
-
-        i++;
-    }
-};
+    
+        // Hard-coded user results from the search
+        users = [
+            new LinkedInUser1("https://www.linkedin.com/pub/andrew-kuang/57/241/a93", p),
+            new LinkedInUser1("https://www.linkedin.com/in/ryanwmchan", p),
+            new LinkedInUser1("https://www.linkedin.com/in/zacharyli323", p)
+        ];
+        console.log(users);
+    
+        //Creates a new google maps marker object for using with the pins
+        while (i < users.length) {
+            //Create a new map marker
+            userLatLng = new google.maps.LatLng(users[i].lat, users[i].long);
+            var Marker = new google.maps.Marker({
+                position: userLatLng,
+                map: _map,
+                icon: 'http://i.imgur.com/NyckpY1.png'
+            });
+            
+            console.log("Getting Linkedin info for user " + i);
+            
+            
+            
+            IN.API.Profile("url=" + users[i].linkedInUrl)
+                .fields(["firstName", "lastName", "headline", "location", "pictureUrl"])
+                .result(function (result) {
+                    console.log(result);
+                    var res = result.values[0];
+                    var firstname = res["firstName"];
+                    var lastname = res["lastName"];
+                    var headline = res["headline"];
+                    var location = res["location"];
+                    var pictureUrl = res["pictureUrl"];
+                    console.log(i + " " + firstname + lastname + headline + location["name"] +
+                               pictureUrl)
+                    
+                    
+                    // Summary box for each searched user
+                    var linkedinUserDivString = firstname + ' ' + lastname + '<br />'
+                                              + headline + '<br />'
+                                              + location["name"] + '<br />';
+                    
+                    //Doesn't work for now (doesn't go into the first if statement
+                    var linkedinUserImgDivString = "";
+                    if (pictureUrl == "undefined"){
+                        linkedinUserImgDivString = '<img src="http://24sessions.com/img/profile_empty.jpg" />';
+                    }
+                    else{
+                        linkedinUserImgDivString = '<img src="' + pictureUrl + '" />';
+                    }
+                    
+                    linkedInResultStrings.push(linkedinUserDivString);
+                    linkedInResultImgStrings.push(linkedinUserImgDivString);
+                    //console.log(linkedInResultStrings);
+                    //console.log(linkedInResultImgStrings);
+                })
+                .error(function (error) {
+                    console.log(error);
+                });
+            
+            google.maps.event.addListener(Marker, 'click', markerClickCallback(i));
+            
+            if (_llbounds === null) {
+                //Create the rectangle in geographical coordinates
+                _llbounds = new google.maps.LatLngBounds(new google.maps.LatLng(p.coords.latitude, p.coords.longitude)); //original
+            } else {
+                //Extends geographical coordinates rectangle to cover current position
+                //_llbounds.extend(userLatLng);
+            }
+            //Sets the viewport to contain the given bounds & triggers the "zoom_changed" event
+            //_map.fitBounds(_llbounds);
+            
+            i++;
+        }
+    };
 
 var fail = function() {
         console.log("Geolocation failed. \nPlease enable GPS in Settings.", 1);
@@ -188,21 +201,21 @@ function onLinkedInAuth() {
 }
 
 function setText() {
-    document.getElementById("user_info").innerHTML = "NULL";
+    document.getElementById("user_info").innerHTML = "HELLO";
 }
 
 function onDeviceReady() {
     try {
         if (device.platform.indexOf("Android") != -1) {
-            intel.xdk.display.useViewport(480, 480);
+            //intel.xdk.display.useViewport(480, 480);
             document.getElementById("map_canvas").style.width = "480px";
         } 
         else if (device.platform.indexOf("iOS") != -1) {
             if (device.model.indexOf("iPhone") != -1 || device.model.indexOf("iPod") != -1) {
-                intel.xdk.display.useViewport(320, 320);
+                //intel.xdk.display.useViewport(320, 320);
                 document.getElementById("map_canvas").style.width = "320px";
             } else if (device.model.indexOf("iPad") != -1) {
-                intel.xdk.display.useViewport(768, 768);
+                //intel.xdk.display.useViewport(768, 768);
                 document.getElementById("map_canvas").style.width = "768px";
             }
         }
